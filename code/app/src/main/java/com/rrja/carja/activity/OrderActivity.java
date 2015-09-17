5112252c2e56e26691aaaf0ca16d8cf55b32052d
wang516483474 <wang516483474@163.com>
2015-09-17 08:30:56 +0800

    a a

--- code/app/src/main/java/com/rrja/carja/activity/OrderActivity.java
+++ code/app/src/main/java/com/rrja/carja/activity/OrderActivity.java
@@ -2,9 +2,11 @@ package com.rrja.carja.activity;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
@@ -30,8 +32,13 @@ import com.rrja.carja.constant.Constant;
 import com.rrja.carja.model.maintenance.MaintenanceOrder;
 import com.rrja.carja.service.DataCenterService;
 import com.rrja.carja.service.impl.OrderBinder;
+import com.rrja.carja.utils.SignUtils;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Random;
 
 public class OrderActivity extends BaseActivity implements View.OnClickListener,
         View.OnFocusChangeListener, TextView.OnEditorActionListener {
@@ -65,6 +72,21 @@ public class OrderActivity extends BaseActivity implements View.OnClickListener,
     private OrderBinder orderService;
 
     private MaintenanceOrder order;
+    private OrderReceiver mReceiver;
+
+    private static final String RSA_PRIVATE = "MIICXgIBAAKBgQDTy1lajyzgd7zVJ5rytlRl9cwt2GlWArIfywIKft+SDnoiufk4"+
+            "NGUKooNJwZOmjK5lhaRIQzfAouB2uSxMZKnBO/yf75p6+EoIgPDaGjqXfDQzycGl"+
+            "MV1hKljeJmMZLENocpF8Dj2Ik4veFt75Fcnu+S13/TEPEGB931AFwbsURQIDAQAB"+
+            "AoGBAJ7VaMx2hYRNp+r/Sb/uJztT6+0R2GVtniIjwAMZBRfnDYePiyywdnpUSDt/"+
+            "FfkYlg2C/SJIbr5kKAxBcMxVzcvELHY6ZovF5ps0vk+9rc8KAlusntnPMTkZpVdE"+
+            "/EaiLB+XVZTpGHvrnryI2n1zFFyTA+wloD6xn2TzlT94S4BdAkEA9as8WcZILQVx"+
+            "m+vvSekMiAxCTXFhRhE2Ww7ERG92CVy7HHfZ2240i0nOoRcP0pYo+0xRqy8h2eJ8"+
+            "TB9sP0cofwJBANyzbz+HJrEmV5zlxFkQR6SuLAFBpHNqQDkh1V3HgvR3m+qNDleY"+
+            "tdL7BM9+nQvrKVGYdk0xVDGcep5x18qzwTsCQCYyn8mdqO9HH2kNAEHPuKEWtuBv"+
+            "tp48YuU5oI67ffDquDUu9XLG6eiWa0hk25L0wh6AuVoSlALa0lTLtfsIx2UCQQC7"+
+            "sttFYzMT7HEM3hicSo0z0HFabDJpeg6+yDiHdlu4gFUZKPfupdDVa2kO8zarYUeV"+
+            "vp22TuK1AskCIf4NxczrAkEAyn7Pt2VndgA/QIxkXGqH5QV8fIXFuuMXq8cw5IyY"+
+            "EUXVfVijXs3KDzDmIomVB/RRD2Ju2uADrjjwju33rGu+hQ==";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -103,6 +125,8 @@ public class OrderActivity extends BaseActivity implements View.OnClickListener,
         Intent intent = new Intent(this, DataCenterService.class);
         intent.setAction(Constant.ACTION_ORDER_SERVICE);
         bindService(intent, conn, BIND_AUTO_CREATE);
+
+        registReceiver();
     }
 
     @Override
@@ -110,6 +134,7 @@ public class OrderActivity extends BaseActivity implements View.OnClickListener,
         if (orderService != null) {
             unbindService(conn);
         }
+        unRegistReceiver();
         super.onDestroy();
     }
 
@@ -562,6 +587,141 @@ public class OrderActivity extends BaseActivity implements View.OnClickListener,
                     break;
             }
         }
+    }
+
+    private class OrderReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            String action = intent.getAction();
+            if (Constant.ACTION_BROADCAST_ORDER_ERR.equals(action)) {
+
+                if (intent.hasExtra("description")) {
+                    String description = intent.getStringExtra("description");
+                    Toast.makeText(context, description, Toast.LENGTH_SHORT).show();
+
+                    btnCommit.setEnabled(true);
+                }
+            }
+
+            if (Constant.ACTION_BROADCAST_ORDER_SUCC.equals(action)) {
+
+                // TODO pay order
+            }
+        }
+    }
+
+    private void registReceiver() {
+        if (mReceiver == null) {
+            IntentFilter filter = new IntentFilter();
+            filter.addAction(Constant.ACTION_BROADCAST_ORDER_SUCC);
+            filter.addAction(Constant.ACTION_BROADCAST_ORDER_ERR);
+
+            mReceiver = new OrderReceiver();
+            registerReceiver(mReceiver, filter);
+        }
+    }
+
+    private void unRegistReceiver() {
+
+        if (mReceiver != null) {
+            unregisterReceiver(mReceiver);
+
+            mReceiver = null;
+        }
+    }
+
+    // ----------------------------------------------------------------------------------------
+
+    private void pay() {
+
+        Stirn
+    }
+
+    private class PayInfo {
+        String tradeNo;
+        double fee;
+        String subject;
+        String body;
+    }
+
+    /**
+     * create the order info. 创建订单信息
+     *
+     */
+    public String getOrderInfo(PayInfo payInfo
+//                               String tradeNo,String subject, String body, String price
+    ) {
+        // 签约合作者身份ID
+        String orderInfo = "partner=" + "\"" + "2088911832390945" + "\"";
+
+        // 签约卖家支付宝账号
+        orderInfo += "&seller_id=" + "\"" + "330201320@qq.com" + "\"";
+
+        // 商户网站唯一订单号
+        orderInfo += "&out_trade_no=" + "\"" + payInfo.tradeNo + "\"";
+
+        // 商品名称
+        orderInfo += "&subject=" + "\"" + payInfo.subject + "\"";
+
+        // 商品详情
+        orderInfo += "&body=" + "\"" + payInfo.body + "\"";
+
+        // 商品金额
+        orderInfo += "&total_fee=" + "\"" + payInfo.fee + "\"";
+
+        // 服务器异步通知页面路径
+//        orderInfo += "&notify_url=" + "\"" + "http://notify.msp.hk/notify.htm"
+//                + "\"";
+       orderInfo += "&notify_url=" + "\""
+               + "http://120.25.201.50/api/order/syncOrderStatus?nattel=" + order.getUserInfo().getTel() + "&authToken="+ order.getUserInfo().getAuthToken() + "&orderNum=" + payInfo.tradeNo + "&status=22"
+               + "\"";
+
+        // 服务接口名称， 固定值
+        orderInfo += "&service=\"mobile.securitypay.pay\"";
+
+        // 支付类型， 固定值
+        orderInfo += "&payment_type=\"1\"";
+
+        // 参数编码， 固定值
+        orderInfo += "&_input_charset=\"utf-8\"";
+
+        // 设置未付款交易的超时时间
+        // 默认30分钟，一旦超时，该笔交易就会自动被关闭。
+        // 取值范围：1m～15d。
+        // m-分钟，h-小时，d-天，1c-当天（无论交易何时创建，都在0点关闭）。
+        // 该参数数值不接受小数点，如1.5h，可转换为90m。
+        orderInfo += "&it_b_pay=\"30m\"";
+
+        // extern_token为经过快登授权获取到的alipay_open_id,带上此参数用户将使用授权的账户进行支付
+//        orderInfo += "&extern_token=" + "\"" + extern_token + "\"";
+
+        // 支付宝处理完请求后，当前页面跳转到商户指定页面的路径，可空
+//        orderInfo += "&return_url=\"m.alipay.com\"";
+
+        // 调用银行卡支付，需配置此参数，参与签名， 固定值 （需要签约《无线银行卡快捷支付》才能使用）
+        // orderInfo += "&paymethod=\"expressGateway\"";
+
+        return orderInfo;
+    }
+
+    /**
+     * sign the order info. 对订单信息进行签名
+     *
+     * @param content
+     *            待签名订单信息
+     */
+    public String sign(String content) {
+        return SignUtils.sign(content, RSA_PRIVATE);
+    }
+
+    /**
+     * get the sign type we use. 获取签名方式
+     *
+     */
+    public String getSignType() {
+        return "sign_type=\"RSA\"";
     }
 
 }
